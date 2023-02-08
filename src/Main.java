4d8374f77786c76d217b2b9d933725eeac595184
Rene <102662801+ReneSambou@users.noreply.github.com>
2023-02-08 11:00:53 +0000

    Update Main.java
    Updated the main class to use arraylists for the options

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -13,30 +14,42 @@ public class Main {
         boolean calculate = true;
         Scanner scan = new Scanner(System.in);
 
-            System.out.println("What do you want to do today:");
+
         while (calculate){
-            System.out.println("What else do you want to do");
-            System.out.println("Enter 1 - Areas of Triangles");
-            System.out.println("Enter 2 - Areas of Rectangle");
-            System.out.println("Enter 3 - Areas of Circles");
-            System.out.println("Enter 4 - For General Arithmetic");
-            System.out.println("Enter 5 - Perimeter of Triangles");
-            System.out.println("Enter 6 - Perimeter of Rectangle");
-            System.out.println("Enter 7 - Perimeter of Circle");
-            System.out.println("Enter 0 - to end program");
-            System.out.print("Your Choice: ");
+            ArrayList<String> option = new ArrayList<String>();
+            option.add("What  do you want to do");
+            option.add("Enter 1 - Areas of Triangles");
+            option.add("Enter 2 - Areas of Rectangle");
+            option.add("Enter 3 - Areas of Circles");
+            option.add("Enter 4 - Areas of Square");
+            option.add("Enter 5 - Perimeter of Square");
+            option.add("Enter 6 - Perimeter of Triangles");
+            option.add("Enter 7 - Perimeter of Rectangle");
+            option.add("Enter 8 - Perimeter of Circle");
+            option.add("Enter 9 - General arithmetic");
+            option.add("Enter 0 - to end program");
+            option.add("Your Choice: ");
+            for(int i =0; i < option.size(); i++){
+                System.out.println(option.get(i));
+            }
             action_today = scan.nextInt();
+
+
+
+
+
+
 
             if (action_today == 1) { // Triangles
                 System.out.print("Enter Base: ");
                 area.base = scan.nextDouble();
                 System.out.print("Enter Height: ");
                 area.height = scan.nextDouble();
-                System.out.println("The Area of your rectangle is: " + area.Triangle());
+                System.out.println("The Area of your Triangle is: " + area.Triangle());
 
             }
             // Perimeter
-            else if (action_today == 5){// Triangle
+            else if (action_today == 6){// Triangle
                 System.out.print("Enter 1st side");
                 perimeter.a = scan.nextDouble();
                 System.out.print("Enter 2nd side");
@@ -46,20 +59,28 @@ public class Main {
                 System.out.println("The perimeter of your triangle is " + perimeter.Triangle());
 
             }
-            else if (action_today == 6){// Rectangle
+            else if (action_today == 7){// Rectangle
                 System.out.print("Enter length: ");
                 perimeter.length = scan.nextDouble();
                 System.out.print("Enter width: ");
                 perimeter.width = scan.nextDouble();
-                System.out.println("The perimeter of your triangle is " + perimeter.Rectangle());
+                System.out.println("The perimeter of your Rectangle is " + perimeter.Rectangle());
             }
 
-            else if (action_today == 7){// Circle
+            else if (action_today == 8){// Circle
                 System.out.print("Enter radius: ");
                 perimeter.radius = scan.nextDouble();
-                System.out.println("The perimeter of your triangle is " + perimeter.Circle());
+                System.out.println("The perimeter of your Circle is " + perimeter.Circle());
             }
+
+            else if (action_today == 5){// Square
+                System.out.print("Enter Length: ");
+                perimeter.length = scan.nextDouble();
+                System.out.println("The perimeter of your Square is " + perimeter.Square());
+            }
+
             else if(action_today == 0) {
+                System.out.println("Thank for your time!!!!");
                 calculate = false;
             }
             else if (action_today == 2) {    // Rectangles
@@ -81,18 +102,33 @@ public class Main {
 
 
             }
-            else if(action_today == 4) {// general Arithmetic
+
+
+
+            else if (action_today == 4) {    // Circles
+                System.out.print("What is the length of Your square? ");
+                area.length = scan.nextDouble();
+                System.out.println("The Area of your square is: " + area.Square());
+
+
+            }
+            else if(action_today == 9) {// general Arithmetic
                 boolean arithmetric = true;
 
                  {
                     System.out.println("What type of operation ");
                      while(arithmetric){
-                    System.out.println("Enter 1- for addition ");
-                    System.out.println("Enter 2- for subtraction ");
-                    System.out.println("Enter 3- for multiplication ");
-                    System.out.println("Enter 4- for division ");
-                    System.out.println("Enter 0 - to go back");
-                    System.out.print("Your Choice: ");
+                         ArrayList<String> options = new ArrayList<String>();
+                         options.add("Enter 1- for addition ");
+                         options.add("Enter 2- for subtraction");
+                         options.add("Enter 3- for multiplication");
+                         options.add("Enter 4- for division");
+                         options.add("Enter 0 - to go back");
+                         options.add("Your Choice: ");
+                         for(int a =0; a < options.size(); a++){
+                             System.out.println(options.get(a));
+
+                         }
                     action_today = scan.nextInt();
 
 
